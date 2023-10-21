e84c8c54259115e49f3fae1f5aedb7b212d97d3a
naterpotatoer <nate.garza@hotmail.com>
2023-10-21 15:50:10 -0700

    adds PropertyCard component

--- src/pages/FormEditor/FormEditor.tsx
+++ src/pages/FormEditor/FormEditor.tsx
@@ -1,10 +1,10 @@
 import { useReducer } from "react";
 import { MOCK_SCHEMA } from "../../mocks/formEditorMock";
 import { schemaReducer } from "../../reducers";
+import PropertyCard from "./components/PropertyCard";
 
 export default function FormEditor() {
   const [schema, dispatch] = useReducer(schemaReducer, MOCK_SCHEMA);
-  const properties = Object.keys(schema.properties);
 
   const handleNewProperty = () => {
     dispatch({
@@ -31,93 +31,15 @@ export default function FormEditor() {
         <button onClick={handleNewProperty}>Add new property</button>
       </div>
       <div>
-        {properties.map((property) => {
-          const isNumberOrInteger = ["number", "integer"].includes(
-            schema.properties[property].type
-          );
-          const isString = schema.properties[property].type === "string";
-          return (
-            <div style={{ margin: "10px" }} key={property}>
-              <label>
-                Key:
-                <input
-                  type="text"
-                  value={property}
-                  onChange={() => {}}
-                  name="key"
-                />
-              </label>
-              <label>
-                Title:
-                <input
-                  type="text"
-                  value={schema.properties[property].title}
-                  onChange={() => {}}
-                  name="title"
-                />
-              </label>
-              <label>
-                Description:
-                <input
-                  type="text"
-                  value={schema.properties[property].description}
-                  onChange={() => {}}
-                  name="description"
-                />
-              </label>
-              <label>
-                Data Type:
-                <select
-                  value={schema.properties[property].type}
-                  onChange={() => {}}
-                  name="dataType"
-                >
-                  <option value="string">String</option>
-                  <option value="number">Number</option>
-                  <option value="integer">Integer</option>
-                  <option value="boolean">Boolean</option>
-                  <option value="object">Object</option>
-                  <option value="array">Array</option>
-                  <option value="null">Null</option>
-                </select>
-              </label>
-              <label>
-                Default:
-                <input
-                  type="text"
-                  value={schema.properties[property].default}
-                  onChange={() => {}}
-                  name="default"
-                />
-              </label>
-              {isNumberOrInteger && (
-                <>
-                  <label>
-                    Minimum:
-                    <input type="number" id="minimum" name="minimum" />
-                  </label>
-                  <label>
-                    Maximum:
-                    <input type="number" id="maximum" name="maximum" />
-                  </label>
-                </>
-              )}
-              {isString && (
-                <>
-                  <label>
-                    Minimum Length:
-                    <input type="number" id="minLength" name="minLength" />
-                  </label>
-                  <label>
-                    Maximum Length:
-                    <input type="number" id="maxLength" name="maxLength" />
-                  </label>
-                </>
-              )}
-              <button onClick={() => handleDelete(property)}>Delete</button>
-            </div>
-          );
-        })}
+        {Object.entries(schema.properties).map(([key, property]) => (
+          <div style={{ margin: "10px" }} key={key}>
+            <PropertyCard
+              propertyKey={key}
+              property={property}
+              handleDelete={handleDelete}
+            />
+          </div>
+        ))}
       </div>
       <pre>{JSON.stringify(schema, null, 2)}</pre>
     </div>
--- src/pages/FormEditor/components/PropertyCard.tsx
+++ src/pages/FormEditor/components/PropertyCard.tsx
@@ -1,15 +1,75 @@
-import React from "react";
 import { JsonSchemaProperty } from "../../../types";
 
-export default function PropertyCard(
-  key: string,
-  property: JsonSchemaProperty
-) {
+export default function PropertyCard({
+  propertyKey,
+  property,
+  handleDelete,
+}: {
+  propertyKey: string;
+  property: JsonSchemaProperty;
+  handleDelete: (key: string) => void;
+}) {
+  const isNumberOrString = ["number", "integer", "string"].includes(
+    property.type
+  );
   return (
-    <div>
-      <h1>
-        {property.title} {key}
-      </h1>
-    </div>
+    <>
+      <label>
+        Data Type:
+        <select value={property.type} onChange={() => {}} name="dataType">
+          <option value="string">String</option>
+          <option value="number">Number</option>
+          <option value="integer">Integer</option>
+          <option value="boolean">Boolean</option>
+          <option value="object">Object</option>
+          <option value="array">Array</option>
+          <option value="null">Null</option>
+        </select>
+      </label>
+      <label>
+        Key:
+        <input type="text" value={propertyKey} onChange={() => {}} name="key" />
+      </label>
+      <label>
+        Title:
+        <input
+          type="text"
+          value={property.title}
+          onChange={() => {}}
+          name="title"
+        />
+      </label>
+      <label>
+        Description:
+        <input
+          type="text"
+          value={property.description}
+          onChange={() => {}}
+          name="description"
+        />
+      </label>
+      <label>
+        Default:
+        <input
+          type="text"
+          value={property.default.toString()} // TODO: verify that this works fine
+          onChange={() => {}}
+          name="default"
+        />
+      </label>
+      {isNumberOrString && (
+        <>
+          <label>
+            Minimum:
+            <input type="number" id="minimum" name="minimum" />
+          </label>
+          <label>
+            Maximum:
+            <input type="number" id="maximum" name="maximum" />
+          </label>
+        </>
+      )}
+      <button onClick={() => handleDelete(propertyKey)}>Delete</button>
+    </>
   );
 }
