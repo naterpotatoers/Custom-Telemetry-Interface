9de562114423ea3d4694950049751945b92a23fe
naterpotatoer <nate.garza@hotmail.com>
2023-11-02 17:25:06 -0700

    adds ability to toggle form and interface views

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,46 @@
-import { useReducer } from "react";
+import { useReducer, useState } from "react";
 import FormEditor from "./pages/FormEditor/FormEditor";
 import InterfaceViewer from "./pages/InterfaceViewer/InterfaceViewer";
 import { MOCK_SCHEMA } from "./mocks";
 import { schemaReducer } from "./reducers";
 
 function App() {
+  const [display, setDisplay] = useState({
+    formEditor: true,
+    interfaceViewer: true,
+  });
   const [schema, dispatch] = useReducer(schemaReducer, MOCK_SCHEMA);
 
   return (
     <div>
-      <h1>Custom Telemetry Controller</h1>
-      {/* <FormEditor schema={schema} dispatch={dispatch} /> */}
-      <InterfaceViewer schema={schema} />
+      <div className="flex-header">
+        <h1>Custom Telemetry Controller</h1>
+        <div>
+          <button
+            name="formEditor"
+            onClick={() =>
+              setDisplay({
+                ...display,
+                formEditor: !display.formEditor,
+              })
+            }
+          >
+            Toggle Form Editor
+          </button>
+          <button
+            onClick={() =>
+              setDisplay({
+                ...display,
+                interfaceViewer: !display.interfaceViewer,
+              })
+            }
+          >
+            Toggle Interface Viewer
+          </button>
+        </div>
+      </div>
+      {display.formEditor && <FormEditor schema={schema} dispatch={dispatch} />}
+      {display.interfaceViewer && <InterfaceViewer schema={schema} />}
     </div>
   );
 }
