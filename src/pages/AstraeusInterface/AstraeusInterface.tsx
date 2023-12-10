d2f8dcd5efd0bcdc2e32db4af3812fd6fe22d268
naterpotatoer <nate.garza@hotmail.com>
2023-12-10 09:14:00 -0800

    :green_heart: removes unused useRef hook

--- src/pages/AstraeusInterface/AstraeusInterface.tsx
+++ src/pages/AstraeusInterface/AstraeusInterface.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { AstraeusType } from "../../types";
 import { getHistoryData } from "./util";
 import { DEFAULT_ASTRAEUS_RESPONSE } from "../../mocks";
